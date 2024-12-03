ea20b01c32cff4189988d764ee3d53a5f3f95cea
PERICLESZA <pericles1011@yahoo.com.br>
2024-12-03 20:06:11 -0300

    Next-Auth configurações

--- auth.ts
+++ auth.ts
@@ -1,6 +1,28 @@
 import NextAuth from "next-auth";
 import Credentials from 'next-auth/providers/credentials'
+import { findUserByCredentials } from "./lib/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Credentials({})],
+  providers: [
+    Credentials({
+      credentials: {
+      email:{},
+      password: {},
+      },
+      authorize: async (credentials) => {
+
+        const email = credentials?.email ?? "";
+        const password = credentials?.password ?? "";
+
+        //lógica de autenticação
+        // procura usuário com credenciais
+        const user = await findUserByCredentials(
+          credentials.email as string, 
+          credentials.password as string)
+        //se não autenticado retorn null
+        //se autenticado, retorna user
+        return user;
+      }
+    })
+  ],
 });
